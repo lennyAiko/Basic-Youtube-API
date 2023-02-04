67d10e6672068594b86fba101ceacc113734d813
Lennox Charles <lennyaiko17@gmail.com>
2023-02-04 15:35:21 +0100

    can add and view videos

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=str, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=str, help="Likes of the video", required=True)
 
-    def get(self):
-        return {"data": "Hello world"}
+videos = {}
 
-api.add_resource(HelloWorld, "/") #(classname, url)
+class Video(Resource):
+
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+api.add_resource(Video, "/video/<int:video_id>") #(classname, url)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "/")
+info = {"likes": 10, "name": "The wild", "views": 120000}
 
+response = requests.put(BASE + "video/1", info)
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
 print(response.json())
