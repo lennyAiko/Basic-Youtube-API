8971aa702eee7c76ff12a4fb0f9f9daa71134680
Lennox Charles <lennyaiko17@gmail.com>
2023-02-04 15:46:00 +0100

    delete added, checker for is exist

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ videos = {}
 
 def abort_if_id_not_exist(id: 'id of the object', storage: 'object location') -> 'checker':
     if id not in storage:
-        abort(404, message="Id is not valid...")
+        abort(404, message="ID is not valid...")
+
+def abort_if_id_exists(id: 'id of the object', storage: 'object location') -> 'checker':
+    if id in storage:
+        abort(409, message="ID already exists...")
 
 class Video(Resource):
 
@@ -22,9 +26,15 @@ class Video(Resource):
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_id_exists(video_id, videos)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+    
+    def delete(self, video_id):
+        abort_if_id_not_exist(video_id, videos)
+        del videos[video_id]
+        return '', 204
 
 api.add_resource(Video, "/video/<int:video_id>") #(classname, url)
 
